e6e3c1c0b78fb1e3c88824035265e8415b2b4641
Gad Grandez <gadgrandez@gmail.com>
2019-01-08 00:46:19 -0500

    Add static for functions

--- lib/oplibrary.dart
+++ lib/oplibrary.dart
@@ -11,18 +11,11 @@ class OPLibrary {
   static final String _opUrl = "https://openlibrary.org";
   static final String _searchOpUrl = '$_opUrl/search.json';
 
-  static bool _initialized = false;
-
-  OPLibrary() {
-    if(_initialized)return;
-    _initialized = true;
-  }
-
   //#region OpenLibrary API Search
-  Future<http.Response> _search(String query) async {
+  static Future<http.Response> _search(String query) async {
     return await http.get(Uri.encodeFull(query), headers: {"Accept": "application/json"});
   }
-  Future<SearchResult> search(String query) async {
+  static Future<SearchResult> search(String query) async {
     String q = '$_searchOpUrl?q=$query';
     http.Response response = await _search(q);
     if(response.statusCode != 200) {
@@ -31,13 +24,13 @@ class OPLibrary {
     SearchResult result = SearchResult.fromMap(json.decode(response.body));
     return result;
   }
-  Future<SearchResult> searchByTitle(String query) async {
+  static Future<SearchResult> searchByTitle(String query) async {
     String q = '$_searchOpUrl?title=$query';
     http.Response response = await _search(q);
     if(response.statusCode != 200) return null;
     return SearchResult.fromMap(json.decode(response.body));
   }
-  Future<SearchResult> searchByAuthor(String query) async {
+  static Future<SearchResult> searchByAuthor(String query) async {
     String q = '$_searchOpUrl?title=$query';
     http.Response response = await _search(q);
     if(response.statusCode != 200) return null;
@@ -45,6 +38,4 @@ class OPLibrary {
   }
   // TODO: Implementar función de búsqueda por páginas
   //#endregion
-
-  static final OPLibrary instance = OPLibrary();
 }
